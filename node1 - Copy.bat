92043fb5e1e5e96a69955ec4f9212319ae522882
Avyakth Kumar <avyakthkumarashok@gmail.com>
2021-12-12 21:29:24 +0530

    all hub setup

---
+++ node1 - Copy.bat
@@ -0,0 +1,1 @@
+java -Dwebdriver.chrome.driver=chromedriver.exe -Dwebdriver.ie.driver=IEDriverServer.exe -Dwebdriver.edge.driver=msedgedriver.exe -jar selenium-server-standalone-3.141.59.jar -role node -hub http://192.168.0.106:4444/grid/register -port 5555 -browser "browserName=chrome,maxInstances=20" -browser "browserName=internet explorer,maxInstances=5"
---
+++ node3.bat
@@ -0,0 +1,1 @@
+java -Dwebdriver.edge.driver=msedgedriver.exe -jar selenium-server-standalone-3.141.59.jar -role node -hub http://192.168.0.106:4444/grid/register -port 5556  -browser "browserName=MicrosoftEdge,maxInstances=10"
